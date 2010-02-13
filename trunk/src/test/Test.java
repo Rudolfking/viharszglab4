72e80090e2a93be4ec08abf3dc4dc6b739e5af0e
arnongoth <arnongoth@43e9ad2e-1713-11df-9fa9-695f62f9af20>
2010-02-13 12:35:13 +0000

    Komment beszúrása, ez is update-e?

--- trunk/src/test/Test.java
+++ trunk/src/test/Test.java
@@ -8,6 +8,6 @@ public class Test {
 	public static void main(String[] args) {
 		System.out.println("Hello World Changed by Szilveszter");
 		System.out.println("Szerintem is Changed by Balázs");
-		System.out.println("Szerintem is Changed by szsz");
+		System.out.println("Szerintem is Changed by szsz");//király, kommentelem is :)
 	}
 }
