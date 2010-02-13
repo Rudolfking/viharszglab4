e8d5c20db9744051d143d832a1dbb7c5a925cea1
arnongoth <arnongoth@43e9ad2e-1713-11df-9fa9-695f62f9af20>
2010-02-13 12:51:06 +0000

    Na szóval: Ha módosítja valaki, amikor/amíg Te is módosítod, és ő előbb updateeli (neki nyilván nem lesz gond), akkor amikor Te töltöd fel (Commit...), akkor Merge Error-t fog dobni. Viszont ha Te frissíted a sajátodat (és benne van a változtatás, amit ugye beleírtál), akkor Update to HEAD esetén összemergeeli a kettőt (letöltött és a Te módosításod), gondolom ha lehetséges (pl. ha Te átírsz valamit, ő meg törli azt, akkor nem tudom, mi van). Érdemes esetleg egy gyors notepad-ba másolás, ha ilyen ütközés lép fel, hátha a másik hülyeséget csinált, a Tied meg emiatt elveszne. Ennyit tudok egyelőre a conflict-ről. Kommenteljétek a hibát, lehet! (belépsz az rXX kattintásával, és ott van review comment lehetőség)

--- src/test/Test.java
+++ src/test/Test.java
@@ -6,7 +6,7 @@ public class Test {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		System.out.println("Hello World Changed by Szilveszter");
+		System.out.println("Hello World Changed by Szilveszter / beleírom, van-e ütközés");//ütközés tesztelése sor
 		System.out.println("Szerintem is Changed by Balázs");
 		System.out.println("Szerintem is Changed by szsz");//király, kommentelem is :)
 
