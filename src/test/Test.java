9a872715750de1a6476b423158fc9f41c55658a2
arnongoth <arnongoth@43e9ad2e-1713-11df-9fa9-695f62f9af20>
2010-02-13 11:29:36 +0000

    Beleírtam még egy kiíratást (println) tesztverzióból.

--- src/test/Test.java
+++ src/test/Test.java
@@ -7,5 +7,6 @@ public class Test {
 	 */
 	public static void main(String[] args) {
 		System.out.println("Hello World Changed by Szilveszter");
+		System.out.println("Szerintem is Changed by Balázs");
 	}
 }
