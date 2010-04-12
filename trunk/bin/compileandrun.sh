7830129045341c5c3ec73b205161cd812e52f349
p0l4rf0x <p0l4rf0x@43e9ad2e-1713-11df-9fa9-695f62f9af20>
2010-04-12 20:51:41 +0000

    cleanuo

--- trunk/bin/compileandrun.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Ez a szkript a szkeletont lefordítja, majd az összes talált
-# teszteset-fájlra lefuttatja.
-# Ubuntu 9.10 alatt tesztelve
-
-# felhasználjuk a két külön szkriptet
-sh ./compile.sh
-sh ./runalltestcases.sh
