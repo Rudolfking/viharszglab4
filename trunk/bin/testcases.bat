cb1182ad127b7e852449c8c1c0c0d8626d0ebf31
p0l4rf0x <p0l4rf0x@43e9ad2e-1713-11df-9fa9-695f62f9af20>
2010-04-13 00:30:24 +0000

    bugfix

--- trunk/bin/testcases.bat
+++ trunk/bin/testcases.bat
@@ -4,40 +4,40 @@ java proto.Proto --no-greeting -i maps\map1.commands -o maps\map1.output
 
 echo "Test case 2"
 java proto.Proto --no-greeting -i maps\map2.commands -o maps\map2.output
-.\check_output maps\map1.output maps\map2.expect
+.\check_output maps\map2.output maps\map2.expect
 
 echo "Test case 3"
 java proto.Proto --no-greeting -i maps\map3.commands -o maps\map3.output
-.\check_output maps\map1.output maps\map3.expect
+.\check_output maps\map3.output maps\map3.expect
 
 echo "Test case 4"
 java proto.Proto --no-greeting -i maps\map4.commands -o maps\map4.output
-.\check_output maps\map1.output maps\map4.expect
+.\check_output maps\map4.output maps\map4.expect
 
 echo "Test case 5"
 java proto.Proto --no-greeting -i maps\map5.commands -o maps\map5.output
-.\check_output maps\map1.output maps\map5.expect
+.\check_output maps\map5.output maps\map5.expect
 
 echo "Test case 6"
 java proto.Proto --no-greeting -i maps\map6.commands -o maps\map6.output
-.\check_output maps\map1.output maps\map6.expect
+.\check_output maps\map6.output maps\map6.expect
 
 echo "Test case 7"
 java proto.Proto --no-greeting -i maps\map7.commands -o maps\map7.output
-.\check_output maps\map1.output maps\map7.expect
+.\check_output maps\map7.output maps\map7.expect
 
 echo "Test case 8"
 java proto.Proto --no-greeting -i maps\map8.commands -o maps\map8.output
-.\check_output maps\map1.output maps\map8.expect
+.\check_output maps\map8.output maps\map8.expect
 
 echo "Test case 9"
 java proto.Proto --no-greeting -i maps\map9.commands -o maps\map9.output
-.\check_output maps\map1.output maps\map9.expect
+.\check_output maps\map9.output maps\map9.expect
 
 echo "Test case 10"
 java proto.Proto --no-greeting -i maps\map10.commands -o maps\map10.output
-.\check_output maps\map1.output maps\map10.expect
+.\check_output maps\map10.output maps\map10.expect
 
 echo "Test case 11"
 java proto.Proto --no-greeting -i maps\map11.commands -o maps\map11.output
-.\check_output maps\map1.output maps\map11.expect
+.\check_output maps\map11.output maps\map11.expect
