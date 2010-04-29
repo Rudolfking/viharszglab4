4707a1ee2a0c107f554097f54ef4a1cd2b6d48e7
nkrisztian89 <nkrisztian89@43e9ad2e-1713-11df-9fa9-695f62f9af20>
2010-04-29 15:15:38 +0000

    Pályafájlok és új osztályok a GUI-hoz. Az src mappából indítsátok java-val. (akkor van jó helyen a maps mappa) Én pl
    javac gui/*.java -d gui/classes -nowarn
    java -classpath gui/classes/ gui/GUI
    
    -val csinálom.

---
+++ src/gui/CityEntryDrawer.java
@@ -0,0 +1,12 @@
+package gui;
+
+import java.awt.*;
+
+public class CityEntryDrawer extends IntersectionDrawer {
+	
+	CityEntryDrawer(Intersection i, int x, int y) {		
+		
+		super(i,x,y);		
+		borderColor = Color.white;		
+	}
+}
