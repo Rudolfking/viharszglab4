ed1ba947390a02f8aa2fd5b6bf9793db463e74d6
nkrisztian89 <nkrisztian89@43e9ad2e-1713-11df-9fa9-695f62f9af20>
2010-05-03 16:29:59 +0000

    A tesztpálya működik. Még nem a végleges verzió!

--- trunk/src/gui/SignDrawer.java
+++ trunk/src/gui/SignDrawer.java
@@ -4,13 +4,17 @@ import java.awt.*;
 
 public abstract class SignDrawer implements IDrawer {
 	
-	final int distFromCenter = 10;
+	final int distFromCenter = 15;
 	
 	protected ISign sign;
 	protected Cell cell;
 	
 	protected int x;
 	protected int y;
+	
+	double max(double a, double b) {
+		return (a > b) ? a : b;
+	}
 	
 	SignDrawer(ISign s, Cell c) {
 		sign = s;
@@ -25,8 +29,8 @@ public abstract class SignDrawer implements IDrawer {
 		double dy = (y2 - y1) / length;		
 		int nCells = cell.getRoad().getCells().length;
 		
-		x = (int)(x2-dy*distFromCenter*2-dx*length/(nCells+1)/2);
-		y = (int)(y2+dx*distFromCenter*2-dy*length/(nCells+1)/2);
+		x = (int)(x2-dy*distFromCenter*2-dx*max(30.0,(length/(nCells+1)/2)));
+		y = (int)(y2+dx*distFromCenter*2-dy*max(30.0,(length/(nCells+1)/2)));
 	}
 	
 	public abstract void draw(Graphics g);		
